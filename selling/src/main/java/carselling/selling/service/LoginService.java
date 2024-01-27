7e3da879378cd1cefc8b929284cf3710aae186fc
MamyAndry <119005054+MamyAndry@users.noreply.github.com>
2024-01-27 15:31:47 +0300

    ok admin

--- selling/src/main/java/carselling/selling/service/LoginService.java
+++ selling/src/main/java/carselling/selling/service/LoginService.java
@@ -67,7 +67,8 @@ public class LoginService implements UserDetailsService {
                 return response;
             }
             user.checkPassWord(password);
-            response.addData("token", jwtUtils.generateJwt(user));
+            user.setPassword(jwtUtils.generateJwt(user));
+            response.addData("token", user);
         }catch(UserException e){
             response.addError("error", e.getMessage());
             response.setStatus(HttpStatus.FORBIDDEN.value());
