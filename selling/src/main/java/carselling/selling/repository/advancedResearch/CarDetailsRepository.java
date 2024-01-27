53c2277afaf4473edff17b7d252333621b58280a
Diana Rakotomaharo <dianarakoto9@gmail.com>
2024-01-27 10:44:37 +0300

    Recherche avancee

---
+++ selling/src/main/java/carselling/selling/repository/advancedResearch/CarDetailsRepository.java
@@ -0,0 +1,7 @@
+package carselling.selling.repository.advancedResearch;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarDetailsRepository extends CrudRepository{
+
+}
