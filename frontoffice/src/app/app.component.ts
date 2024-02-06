ff499ed468a851c52d86e43d8849ddba5ce9eaf1
olinaf11 <fanilokevin@icloud.com>
2024-02-06 16:11:05 +0300

    add frontoffice

--- frontoffice/src/app/app.component.ts
+++ frontoffice/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AnnouncementListComponent } from "./components/announcement-list/announcement-list.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, RouterLink, RouterLinkActive, AnnouncementListComponent]
 })
 export class AppComponent {
   title = 'frontoffice';
--- frontoffice/src/app/app.config.ts
+++ frontoffice/src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+    providers: [
+      provideRouter(routes),
+      importProvidersFrom(HttpClientModule,FormsModule,BrowserAnimationsModule)
+    ]
 };
